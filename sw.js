47c575b23357820c3c7745a2e413306082328fb0
miguelcalvell <miguelcalvell@gmail.com>
2025-10-30 11:14:49 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// bump cache to v6.10 to force refresh
+// Service worker cache bumped to v6.11-ml to force refresh
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('appendix-batcher-v6.10').then(cache => cache.addAll([
+  e.waitUntil(caches.open('appendix-batcher-v6.11-ml').then(cache => cache.addAll([
     './', './index.html', './style.css', './app.js',
     'https://unpkg.com/pdf-lib@1.17.1/dist/pdf-lib.min.js',
     'https://cdn.jsdelivr.net/npm/jszip@3.10.1/dist/jszip.min.js'
